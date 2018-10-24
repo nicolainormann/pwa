f8e7e6033b97a98c9692c7cef9afadeb424a0115
Nicolai Normann <nicolainormann90@gmail.com>
2018-10-24 23:32:24 +0200

    I don't know

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,20 @@
 import { API_KEY } from "./api.key";
 
+import { get, set } from "idb-keyval";
+
 export class Api {
     private static _apiUrl(endpoint: string, page: number, query?: string) {
         return `https://newsapi.org/v2/${endpoint}?apiKey=${API_KEY}&page=${page}${query ? "&q=" + query : ""}`;
     }
 
-    static getNews(page = 1, query = "*") {
-        return fetch(this._apiUrl("everything", page, query))
-            .then(res => res.json() as Promise<IResponse>);
+    static async getNews(page = 1, query = "*") {
+        const stored = await get("news");
+        if(stored) {
+            return stored as Promise<IResponse>;
+        }
+        const res = await fetch(this._apiUrl("everything", page, query));
+        const json = await res.json() as Promise<IResponse>;
+        set("news", json);
+        return json;
     }
 }
